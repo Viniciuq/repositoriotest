1caaf10a020523187f3bcae95c315f072179a1df
Vinicius Lustosa <vinisilva54541@gmail.com>
2024-10-27 01:03:35 -0300

    adicionando comentario ao qualquercoisa

--- qualquercoisa.v
+++ qualquercoisa.v
@@ -2,8 +2,10 @@ module semafaro(A, VerdeA, VerdeB);
 
     input A;
     output VerdeA, VerdeB;
-    
+        
     assign VerdeA = A;
-    assign VerdeB = ~A;
+    assign VerdeB = ~A; 
+
+    //adicionar nova função
 
 endmodule
